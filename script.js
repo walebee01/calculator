333b24c408b98ef702dbefd7aa9701fe21dc7dd6
Wale Osho <90667947+walebee01@users.noreply.github.com>
2022-10-01 13:34:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+
+const display = document.querySelector('.display');
+const controlKeys = document.querySelector('.control-keys').children;
+
+let allSymbols = ['+', '-', '/', 'x', '%', 'C', '='];
+
+
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+let result ='';
+
+const calculate = () => {
+     firstNum = parseFloat(firstNum);
+     secondNum = parseFloat(secondNum);
+
+    if (operator === '+') result = firstNum + secondNum
+    if (operator === '-') result = firstNum - secondNum
+    if (operator === '/') result = firstNum / secondNum
+    if (operator === 'x') result = firstNum * secondNum
+    if (operator === '%') result = firstNum % secondNum
+
+    display.innerText = result
+    firstNum = result
+    secondNum = ''
+}
+
+for (let key of controlKeys){
+    key.addEventListener('click', ()=>{
+        const { innerText: keysValue } = key
+        const btnValueSymbol =allSymbols.includes(keysValue)
+
+if(!secondNum && keysValue === '=') return null
+
+if(keysValue == 'C'){
+    firstNum = ''
+    secondNum =''
+    symbol = ''
+    return display.innerText=''
+}
+
+
+if (firstNum && btnValueSymbol){
+    secondNum && calculate()
+    operator = keysValue
+}
+else if (!operator) firstNum += keysValue
+else if (operator) secondNum += keysValue
+
+
+      if (keysValue !== '=' )  display.innerText += keysValue;
+    })
+}
+
